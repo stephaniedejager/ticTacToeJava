978b2004d3d8414af7c09115cffa05f14afb08ff
Stephanie de Jager <sdejager.za@gmail.com>
2022-05-06 13:48:17 +0200

    Create gameboard

--- TicTacToe/src/ticTacToe/TicTacToe.java
+++ TicTacToe/src/ticTacToe/TicTacToe.java
@@ -3,6 +3,22 @@ package ticTacToe;
 public class TicTacToe {
 
     public static void main(String[] args) {
-	// write your code here
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+        printGameBoard(gameBoard);
+
     }
+
+    public static void printGameBoard(char[][] gameBoard){
+        for(char[] row: gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
 }
